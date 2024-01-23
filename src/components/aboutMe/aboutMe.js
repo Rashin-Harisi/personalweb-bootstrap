f34b786f6b4606e413dcb9ee23a8fb0b12a6a341
Rashin Harisi <rashin.harisi@gmail.com>
2024-01-23 16:56:46 +0100

    Updating the aboutMe component

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './aboutMe.css';
 
 
 const AboutMe= ()=>{
     return (
-        <div></div>
+        <div  id='aboutMe' style={{width:'700px', margin:'auto', marginTop: '20px', textAlign:'justify', fontSize:'16px',}}>
+            I am <em>Rashin Harisi</em>, holding an MSc. in Biomedical Engineering. Having shifted my career path last year, I dedicated myself to acquiring the skills necessary to become a proficient Front End Developer, focusing on HTML, CSS, JavaScript, and React. You can explore my journey through the certificates I earned from FreeCodeCamp (FCC). As an aspiring junior front-end developer, I bring a unique combination of creativity and technical acumen to craft compelling and user-friendly web interfaces. My commitment extends to staying updated on emerging technologies and frameworks, enriching my skill set to contribute effectively to collaborative projects. Eager to learn and driven to excel, I am poised to embrace the challenges of the dynamic front-end development landscape, channeling my enthusiasm into innovative and impactful projects.
+        </div>
     )
 };
 
