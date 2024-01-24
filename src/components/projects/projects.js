92b949cf203ba7b54a37222540b426d492945744
Rashin Harisi <rashin.harisi@gmail.com>
2024-01-24 09:24:33 +0100

    Updating the Projects component

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const Projects =()=>{
+const Projects = () => {
     return (
-        <div></div>
+        <div className='projectDetails' style={{height: '150px'}}>
+            <p style={{fontFamily: 'Apple Chancery , cursive', marginTop:'40px'}}>
+                I am still working on my portfolio. Thanks for your patience.<br />
+                You can find my projects in my Github profile. To see <a href='https://github.com/Rashin-Harisi'>click here</a>.
+
+            </p>
+
+        </div>
     )
 };
 
